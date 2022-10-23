8c12934e5cda3c709cd9b0853b329f30044adfab
Eddy <ek@snoke.de>
2022-10-23 22:15:57 +0200

    fixing pom version

--- src/test/java/com/starhotel/RoomReservation/RoomReservationApplicationTests.java
+++ src/test/java/com/starhotel/RoomReservation/RoomReservationApplicationTests.java
@@ -3,6 +3,7 @@ package com.starhotel.RoomReservation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class RoomReservationApplicationTests {
 
 	@Test
