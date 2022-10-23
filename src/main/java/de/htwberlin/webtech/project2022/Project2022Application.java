23ccaf88041e02a9dc32c9d47aa20adeb08ea9ca
Eddy <ek@snoke.de>
2022-10-23 18:00:19 +0200

    init

---
+++ src/main/java/de/htwberlin/webtech/project2022/Project2022Application.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.project2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/de/htwberlin/webtech/project2022/Project2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htwberlin.webtech.project2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
